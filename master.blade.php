231ccc854caa5a5227cbde7129cdbdcfbde20673
fahimtayebee9 <fahimtayebee9@gmail.com>
2021-03-31 00:26:24 +0600

    responsive

---
+++ master.blade.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="keywords" content="MediaCenter, Template, eCommerce">
+    <meta name="robots" content="all">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Aimsbay</title>
+    <link rel="shortcut icon" href="assets/img/Favicon.png" type="image/x-icon">
+
+    <!-- <script src="assets/lib/aos/js/aos.js"></script> -->
+
+    <link rel="stylesheet" href="assets/lib/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/lib/@fortawesome/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/lib/jquery/jquery.min.js">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/hover.css">
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" href="assets/css/keyframe.css">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <link rel="stylesheet" href="assets/lib/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/lib/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/flaticon.css">
+
+</head>
+
+<body onscroll="scroll()">
+    <!-- HEADER AND BANNER START -->
+    <header>
+        <nav id="menu"
+            class="sticky-header navbar navbar-expand-lg navbar-light navbar-back justify-content-between black aos-init aos-animate"
+            data-aos="fade-right">
+            <div class="container">
+                <a class="navbar-brand" href="index.html"><img src="assets/img/logo.png" class="header-logo"
+                        alt=""></a>
+                <button class="navbar-toggler btn-navbar" onclick="btnClick()" type="button" id="btn-nav"
+                    data-toggle="collapse" data-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon icon-nav"></span>
+                </button>
+                <div class="collapse navbar-collapse collapse-ul" id="navbarSupportedContent">
+
+                    <ul id="" class="navbar-nav ml-auto ">
+                        <li class="nav-item ml-1 mr-1">
+                            <a class="nav-link hover-ani active hvr-shutter-out-vertical" href="#">
+                                <span class="hover-text">Home</span></a>
+                        </li>
+                        <li class="nav-item dropdown ml-1 mr-1">
+
+                            <a class="nav-link dropdown-toggle hover-ani hvr-shutter-out-vertical" href="#"
+                                id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                Our services
+                                <span class="hover-icon">
+                                    <i class="fas fa-chevron-right"></i>
+                                </span>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="software-show.html">Software Development</a>
+                                <div class="dropdown-divider"></div>
+                            </div>
+                        </li>
+                        <li class="nav-item  ml-1 mr-1">
+                            <a class="nav-link hover-ani hvr-shutter-out-vertical" href="about-us.html">About Us</a>
+                        </li>
+                        <li class="nav-item dropdown  ml-1 mr-1">
+                            <a class="nav-link dropdown-toggle hover-ani hvr-shutter-out-vertical" href="#"
+                                id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                Learn More
+                                <span class="hover-icon">
+                                    <i class="fas fa-chevron-right"></i>
+                                </span>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="contact.html">Contact Us</a>
+                                <a class="dropdown-item" href="#">Testimonials</a>
+                            </div>
+                        </li>
+                        <li class="nav-item  ml-1 mr-1">
+                            <a class="nav-link hover-ani hvr-shutter-out-vertical" href="client.html"><span
+                                    class="hover-text">Our Clients</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <!-- HEADER END -->
+
+
+    <!-- FOOTER SECTION -->
+    <section class="footer ">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12 footer-head">
+                    <img src="assets/img/logo.png" class="img-fluid w-15" alt="">
+                </div>
+            </div>
+
+            <div class="row pt-4">
+                <div class="col-md-4 col-lg-4 col-sm-10 text-center footer-menu">
+                    <h4 class="title">Main Office</h4>
+                    <div class="info">
+                        <div class="contact-info">
+                            <p class="mb-2">Olokar More,<br>Rajshahi-6100,<br>Bangladesh</p>
+                            <p class="d-flex justify-content-between"><span>Phone:</span> <span>+8801798492343</span>
+                            </p>
+                            <p class="d-flex justify-content-between"><span>Email:</span> <span>hr@aimsbay.com</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-lg-4 col-sm-10 text-center footer-menu">
+                    <h4 class="title">City Office</h4>
+                    <div class="info">
+                        <div class="contact-info">
+                            <p class="mb-2">Olokar More,<br>Rajshahi-6100,<br>Bangladesh</p>
+                            <p class="d-flex justify-content-between"><span>Phone:</span> <span>+8801798492343</span>
+                            </p>
+                            <p class="d-flex justify-content-between"><span>Email:</span> <span>hr@aimsbay.com</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 col-lg-4 col-sm-10 text-center footer-menu">
+                    <h4 class="title">Usefull links</h4>
+                    <div class="info">
+                        <ul class="m-3">
+                            <li><a href="">Software Development</a></li>
+                            <li><a href="">Software Development</a></li>
+                            <li><a href="">Software Development</a></li>
+                            <li><a href="">Software Development</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer>
+            <div class="container pb-3 pt-3">
+                <div class="row">
+                    <div class="col-md-8 col-lg-8 col-sm-10 m-auto text-center">
+                        <p class="m-0">Copyright © 2021 <span class="footer-icon">AimsBay</span>. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        
+    </section>
+
+    <!-- FOOTER SECTION -->
+
+
+    <!-- SCRIPTS SECTION -->
+    <script src="assets/js/script.js"></script>
+    <script src="assets/lib/jquery/jquery.slim.min.js"></script>
+    <script src="assets/lib/popper.js/popper.min.js"></script>
+    <script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/lib/wow/wow.js"></script>
+    <script src="assets/lib/OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
+    <script src="assets/lib/aos/js/aos.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $(".dropdown").hover(
+                function () {
+                    $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+                    $(this).toggleClass('open');
+                },
+                function () {
+                    $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+                    $(this).toggleClass('open');
+                }
+            );
+        });
+    </script>
+
+    <script>
+        new WOW().init();
+    </script>
+
+    <script>
+        AOS.init();
+
+        $('.owl-carousel').owlCarousel({
+            stagePadding: 50,
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                320: {
+                    items: 1,
+                    stagePadding: 70
+                },
+                600: {
+                    items: 2,
+                    stagePadding: 60
+                }
+            },
+            navText: ["<i class='far fa-arrow-alt-circle-left'></i>", "<i class='far fa-arrow-alt-circle-right'></i>"]
+        })
+    </script>
+
+</body>
+
+</html>
